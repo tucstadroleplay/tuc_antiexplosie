6da60e1b65d602db8a7947481e788655682b370c
Tucstad Roleplay <65197482+tucstadroleplay@users.noreply.github.com>
2020-05-29 13:52:33 +0200

    Explosie (Voertuig) opruimen toegevoegd!
    Explosie (Voertuig) opruimen toegevoegd!

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,10 @@ games { 'rdr3', 'gta5' }
 author 'MeistroBurger#0001 (Tucstad Roleplay)'
 description 'Schakelt Explosies uit in de Server'
 
+client_scripts {
+	'client.lua'
+}
+
 server_scripts {
 	'server.lua'
 }
